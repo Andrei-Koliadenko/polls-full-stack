d1c64ce975199646dad4a0e890d1da096414d000
andrei-koliadenko <iamandriikol@gmail.com>
2021-01-24 19:17:20 +0200

    Update service

--- client/src/services/PollsServiceObservable.ts
+++ client/src/services/PollsServiceObservable.ts
@@ -1,5 +1,9 @@
 import SimplePoll from "../models/SimplePoll";
+import {Observable} from "rxjs";
+import SimplePollAndVotes from "../models/SimplePollAndVotes";
 
 export default interface PollsServiceObservable {
     createSimplePoll(poll: SimplePoll): Promise<any>;
+    
+    getSimplePoll(pollId: string): Observable<SimplePollAndVotes>;
 }
--- client/src/services/PollsServiceObservableRestImpl.ts
+++ client/src/services/PollsServiceObservableRestImpl.ts
@@ -2,6 +2,23 @@ import {Axios} from "axios-observable";
 import PollsServiceObservable from "./PollsServiceObservable";
 import Poll from "../models/Poll";
 import SimplePoll from "../models/SimplePoll";
+import {Observable, throwError} from "rxjs";
+import SimplePollAndVotes from "../models/SimplePollAndVotes";
+import {catchError, map} from "rxjs/operators";
+import {AxiosError} from "axios";
+import ErrorTypes from "../models/ErrorTypes";
+
+function handleError(err: AxiosError): ErrorTypes {
+    if (err.response) {
+        if (err.response.status === 401 || err.response.status === 403) {
+            return ErrorTypes.AUTH_ERROR;
+        } else {
+            return ErrorTypes.SERVER_ERROR;
+        }
+    } else {
+        return ErrorTypes.NETWORK_ERROR;
+    }
+}
 
 export default class PollsServiceObservableRestImpl implements PollsServiceObservable {
     constructor(private url: string) {
@@ -10,5 +27,19 @@ export default class PollsServiceObservableRestImpl implements PollsServiceObser
 
     createSimplePoll(poll: SimplePoll): Promise<any> {
         return Axios.post<Poll>(this.url + '/create', poll).toPromise();
+    }
+
+    getSimplePoll(pollId: string): Observable<SimplePollAndVotes> {
+        return Axios.get<SimplePollAndVotes>(this.url, {
+            params: {
+                id: pollId
+            }
+        })
+            .pipe(
+                map(response => response.data),
+                catchError(err => {
+                    return throwError(handleError(err));
+                })
+            );
     }
 }
