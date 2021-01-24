44dba2f1b19696631029c300635088c28479b205
andrei-koliadenko <iamandriikol@gmail.com>
2021-01-24 14:40:57 +0200

    Add spring boot application

---
+++ server/src/main/java/com/techtask/pollscreation/PollsCreationApplication.java
@@ -0,0 +1,13 @@
+package com.techtask.pollscreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollsCreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollsCreationApplication.class, args);
+	}
+
+}
