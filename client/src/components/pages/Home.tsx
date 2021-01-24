32bc66b016664d4806471a9869659326fe74f379
andrei-koliadenko <iamandriikol@gmail.com>
2021-01-24 03:35:37 +0200

    Update Home page

--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -1,35 +1,133 @@
 import React, {FC, useState} from "react";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import CardActions from "@material-ui/core/CardActions";
-import Button from "@material-ui/core/Button";
 import PollDialog from "../page-elements/PollDialog";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
+
+const images = [
+    {
+        url: 'https://i.ibb.co/WFXr96p/Election-or-referendum-campaign-Citizens-voting-for-candidates-inserting-survey-form-to-ballot-box-V.jpg',
+        title: 'Create poll',
+        width: '100%',
+    },
+];
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            display: 'flex',
+            flexWrap: 'wrap',
+            minWidth: 300,
+            width: '100%',
+        },
+        image: {
+            position: 'relative',
+            minHeight: 630,
+            height: '100%',
+            [theme.breakpoints.down('xs')]: {
+                width: '100% !important', // Overrides inline-style
+                height: 100,
+            },
+            '&:hover, &$focusVisible': {
+                zIndex: 1,
+                '& $imageBackdrop': {
+                    opacity: 0.15,
+                },
+                '& $imageMarked': {
+                    opacity: 0,
+                },
+                '& $imageTitle': {
+                    border: '4px solid currentColor',
+                },
+            },
+        },
+        focusVisible: {},
+        imageButton: {
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: theme.palette.common.white,
+        },
+        imageSrc: {
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center 40%',
+        },
+        imageBackdrop: {
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            backgroundColor: theme.palette.common.black,
+            opacity: 0.4,
+            transition: theme.transitions.create('opacity'),
+        },
+        imageTitle: {
+            position: 'relative',
+            padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${theme.spacing(1) + 6}px`,
+        },
+        imageMarked: {
+            height: 3,
+            width: 18,
+            backgroundColor: theme.palette.common.white,
+            position: 'absolute',
+            bottom: -2,
+            left: 'calc(50% - 9px)',
+            transition: theme.transitions.create('opacity'),
+        },
+    }),
+);
+
 
 const Home: FC = () => {
     const [openCreatePollDialog, setCreatePollDialog] = useState<boolean>(false);
+    const classes = useStyles();
 
-    return (
-        <div style={{
-            position: 'absolute', left: '50%', top: '50%',
-            transform: 'translate(-50%, -50%)'
-        }}>
-            <Card>
-                <CardContent>
-                    <Typography variant={"h5"}>
-                        This application allows you to create a poll and share it
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <Button size="large" variant="contained" color="primary" fullWidth
-                            onClick={() => setCreatePollDialog(true)}>
-                        Create poll
-                    </Button>
-                </CardActions>
-            </Card>
-            <br/>
-            {openCreatePollDialog &&
-            <PollDialog openLoginFormDialog={openCreatePollDialog} setOpenLoginFormDialog={setCreatePollDialog}/>}
-        </div>)
+    return (<div className={classes.root}>
+        {images.map((image) => (
+            <ButtonBase
+                focusRipple
+                key={image.title}
+                className={classes.image}
+                focusVisibleClassName={classes.focusVisible}
+                style={{
+                    width: image.width,
+                }}
+                onClick={()=> setCreatePollDialog(true)}
+            >
+          <span
+              className={classes.imageSrc}
+              style={{
+                  backgroundImage: `url(${image.url})`,
+              }}
+          />
+                <span className={classes.imageBackdrop}/>
+                <span className={classes.imageButton}>
+            <Typography
+                component="span"
+                variant="h1"
+                color="inherit"
+                className={classes.imageTitle}
+            >
+              {image.title}
+                <span className={classes.imageMarked}/>
+            </Typography>
+          </span>
+            </ButtonBase>
+        ))}
+
+        {openCreatePollDialog &&
+        <PollDialog openLoginFormDialog={openCreatePollDialog} setOpenLoginFormDialog={setCreatePollDialog}/>}
+    </div>)
 }
 export default Home
