8b3589a0d288a374061e032fb4d19928e05348b0
andrei-koliadenko <iamandriikol@gmail.com>
2021-01-26 17:03:53 +0200

    Update subscribing.

--- client/src/components/pages/DisplayPoll.tsx
+++ client/src/components/pages/DisplayPoll.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useState} from "react";
+import React, {FC, useEffect, useState} from "react";
 import {servicePolls} from "../../config/server-config";
 import {Observable} from "rxjs";
 import SimplePollAndVotes from "../../models/SimplePollAndVotes";
@@ -10,12 +10,17 @@ interface RouteParams {
 }
 
 const DisplayPoll: FC = () => {
+    const [poll, setPoll] = useState<SimplePollAndVotes>();
     const {id} = useParams<RouteParams>();
 
-    const response: Observable<SimplePollAndVotes> = servicePolls.getSimplePoll(id);
-    const [poll, setPoll] = useState<SimplePollAndVotes>();
+    useEffect(()=> {
+        const response: Observable<SimplePollAndVotes> = servicePolls.getSimplePoll(id);
+        response.subscribe(data => {
+            setPoll(data)
+            console.log(data)
+        }, error => console.log(error));
+    },[])
 
-    response.subscribe(data => setPoll(data), error => console.log(error));
     return <React.Fragment>
         <VoteCard poll={poll}/>
     </React.Fragment>
