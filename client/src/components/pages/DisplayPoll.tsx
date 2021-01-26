6b59b4c3c43079ac4a227e0bdf19b905cbbba29a
andrei-koliadenko <iamandriikol@gmail.com>
2021-01-26 17:45:55 +0200

    Update subscribing. Add poller

--- client/src/components/pages/DisplayPoll.tsx
+++ client/src/components/pages/DisplayPoll.tsx
@@ -1,25 +1,46 @@
 import React, {FC, useEffect, useState} from "react";
 import {servicePolls} from "../../config/server-config";
-import {Observable} from "rxjs";
+import {Subscription} from "rxjs";
 import SimplePollAndVotes from "../../models/SimplePollAndVotes";
 import {useParams} from "react-router";
 import VoteCard from "../Cards/VoteCard";
 
 interface RouteParams {
-    id: string
+    id: string,
 }
 
 const DisplayPoll: FC = () => {
     const [poll, setPoll] = useState<SimplePollAndVotes>();
     const {id} = useParams<RouteParams>();
 
-    useEffect(()=> {
-        const response: Observable<SimplePollAndVotes> = servicePolls.getSimplePoll(id);
-        response.subscribe(data => {
-            setPoll(data)
-            console.log(data)
-        }, error => console.log(error));
-    },[])
+    useEffect(() => {
+        let intervalId: any;
+        let subscription: Subscription;
+        intervalId = setInterval(poller, 2000);
+        getData();
+
+        function poller() {
+            if (!subscription || subscription.closed) {
+                getData();
+            }
+        }
+
+        function getData() {
+            subscription = servicePolls.getSimplePoll(id).subscribe(data => {
+                setPoll(data)
+                console.log(data)
+            }, error => console.log(error));
+        }
+
+        return () => {
+            if (subscription && !subscription.closed) {
+                subscription.unsubscribe();
+            }
+            if (intervalId) {
+                clearInterval(intervalId)
+            }
+        }
+    }, [id])
 
     return <React.Fragment>
         <VoteCard poll={poll}/>
