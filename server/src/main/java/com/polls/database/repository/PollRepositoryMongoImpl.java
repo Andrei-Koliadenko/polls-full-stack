0d623bd0338972bef00140ac19884a34fd5869f3
Andrei Koliadenko <iamandriikol@gmail.com>
2021-12-25 02:02:46 +0200

    mongo _id instead of String it is ObjectId

--- server/src/main/java/com/polls/database/repository/PollRepositoryMongoImpl.java
+++ server/src/main/java/com/polls/database/repository/PollRepositoryMongoImpl.java
@@ -1,9 +1,9 @@
 package com.polls.database.repository;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.polls.database.model.PollDocument;
 
-public interface PollRepositoryMongoImpl extends MongoRepository<PollDocument, String> {
-
+public interface PollRepositoryMongoImpl extends MongoRepository<PollDocument, ObjectId> {
 }
