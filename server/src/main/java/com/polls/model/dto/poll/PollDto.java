f07dc2ced741d20e7f8bba47c6fac7bb5884f9f9
Andrei Koliadenko <iamandriikol@gmail.com>
2021-12-30 14:19:43 +0200

    add validation on id field

--- server/src/main/java/com/polls/model/dto/poll/PollDto.java
+++ server/src/main/java/com/polls/model/dto/poll/PollDto.java
@@ -3,9 +3,12 @@ package com.polls.model.dto.poll;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.Null;
+
 @Data
 @Builder
 public class PollDto {
+    @Null
     private String id;
     private String pollName;
     private QuestionDto question;
