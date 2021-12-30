0513b48eef03c762a75ecc1c04585330d1e175b1
Andrei Koliadenko <iamandriikol@gmail.com>
2021-12-30 13:29:24 +0200

    add toAnswerOptionDocument. Initial votesCount is always 0

--- server/src/main/java/com/polls/mappers/PollMapper.java
+++ server/src/main/java/com/polls/mappers/PollMapper.java
@@ -1,14 +1,23 @@
 package com.polls.mappers;
 
+import com.polls.database.model.AnswerOptionDocument;
 import com.polls.database.model.PollDocument;
+import com.polls.model.dto.poll.AnswerOptionDto;
 import com.polls.model.dto.poll.PollDto;
+import org.bson.types.ObjectId;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(uses = MapperUtils.class, config = MapperConfiguration.class)
+@Mapper(uses = MapperUtils.class, config = MapperConfiguration.class, imports = ObjectId.class)
 public interface PollMapper {
     PollDocument toPollDocument(PollDto poll);
 
-    @Mapping(target = "question.totalVotes", source = "question.answers")
+    @Mapping(source = "question.answers", target = "question.totalVotes")
     PollDto toPollDto(PollDocument poll);
+
+    @Mapping(target = "id", expression = "java(new ObjectId())")
+    @Mapping(target = "votesCount", constant = "0")
+    AnswerOptionDocument toAnswerOptionDocument(AnswerOptionDto answerOption);
+
+
 }
