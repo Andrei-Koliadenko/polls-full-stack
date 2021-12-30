884280ce7aebef506d97b9888b9cdf05d12053c0
Andrei Koliadenko <iamandriikol@gmail.com>
2021-12-30 20:10:20 +0200

    remove unnecessary public

--- server/src/main/java/com/polls/database/operations/PollMongoOperations.java
+++ server/src/main/java/com/polls/database/operations/PollMongoOperations.java
@@ -4,5 +4,5 @@ import com.polls.database.model.PollDocument;
 import com.polls.model.dto.vote.VoteDto;
 
 public interface PollMongoOperations {
-    public PollDocument addVote(String pollId, VoteDto vote);
+    PollDocument addVote(String pollId, VoteDto vote);
 }
