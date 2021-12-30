bd1580513b57963207211546500b630f651651cb
Andrei Koliadenko <iamandriikol@gmail.com>
2021-12-30 23:57:29 +0200

    move from @RestControllerAdvice to @ControllerAdvice + @ResponseBody

--- server/src/main/java/com/polls/exeptions/GlobalExceptionsHandler.java
+++ server/src/main/java/com/polls/exeptions/GlobalExceptionsHandler.java
@@ -1,52 +1,59 @@
 package com.polls.exeptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolationException;
 
-import org.slf4j.*;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionsHandler {
 
 	private static Logger LOG = LoggerFactory.getLogger(GlobalExceptionsHandler.class);
 
 	@ExceptionHandler(ConstraintViolationException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ResponseBody
 	String processingConstraintExceptions(ConstraintViolationException e, HttpServletRequest request) {
 		return processingException(e);
 	}
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ResponseBody
 	String processingMethodArgumentExceptions(MethodArgumentNotValidException e, HttpServletRequest request) {
 		return processingException(e);
 	}
 
 	@ExceptionHandler(InvalidInputException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ResponseBody
 	String processingInvalidInputExceptions(InvalidInputException e) {
 		return processingException(e);
 	}
 
 	@ExceptionHandler(NotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ResponseBody
 	String processingNotFoundExceptions(NotFoundException e) {
 		return processingException(e);
 	}
 
 	@ExceptionHandler(RuntimeException.class)
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	@ResponseBody
 	String processingOtherExceptions(RuntimeException e) {
 		return processingException(e);
 	}
 	
 	private String processingException(Exception e) {
 		LOG.error(e.getMessage());
-		return e.getMessage();
+		return e.getLocalizedMessage();
 	}
 }
